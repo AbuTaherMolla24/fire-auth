3d7a84a1335cb911a0a7d3f7333ae360303e02c0
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-09-09 12:32:36 +0600

    end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
 
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig)
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    phone: ''
+  })
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(response => {
+      const {displayName, photoURL, email} = response.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      }
+      setUser(signedInUser);
+
+      console.log(displayName, email, photoURL);
+    })
+    .catch(error => {
+      console.log(error);
+      console.log(error.message);
+    })
+  }
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+    .then(response => {
+      const signedOutUser = {
+        isSignedIn: false,
+        name: '',
+        photo: '',
+        email: ''
+      }
+      setUser(signedOutUser);
+    })
+    .catch(err => {
+
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+       user.isSignedIn ?  <button onClick={handleSignOut}> Sign out</button> :
+      <button onClick={handleSignIn}> Sign in</button>}
+     {
+       user.isSignedIn && <div>
+       <p>Welcome, {user.name}!</p>
+       <p>your email: {user.email}</p>
+        <img src={user.photo} alt=''></img>
+       </div>
+     }
     </div>
   );
 }
